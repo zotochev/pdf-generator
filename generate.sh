6926c2d89c8d9d92978e95cb044b572867234f7c
zotochev <mikhail@digitalcube.rs>
2023-03-15 14:21:40 +0100

    added init script

--- generate.sh
+++ generate.sh
@@ -5,8 +5,10 @@ TEMPLATE_PATH="templates/$TEMPLATE"
 RESULT_PATH="results/$TEMPLATE"
 DATA_PATH="./invoice_data.json"
 
+source venv/bin/activate
+
 # generate html from template
 python main.py "$TEMPLATE_PATH" -d "$DATA_PATH"
 
 # generate pdf from html
- weasyprint -s "$RESULT_PATH/invoice.css" "$RESULT_PATH/index.html" results/invoice-$(date +"%d%m%Y%H%M%S").pdf
+weasyprint -s "$RESULT_PATH/invoice.css" "$RESULT_PATH/index.html" results/invoice-$(date +"%d%m%Y%H%M%S").pdf
